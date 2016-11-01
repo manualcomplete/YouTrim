c5ddcedf8c796c2f78841101011738bd8c26ef4c
jytc <joychen17@gmail.com>
2016-11-01 10:31:36 -0400

    Initial commit YouLoop.

---
+++ YouLoop.py
@@ -0,0 +1,81 @@
+# Script to download youtube video at url and trim it to the specified length
+
+import argparse
+import os
+import subprocess
+from arrow.parser import ParserError
+import arrow
+
+def get_time(time):
+	formats = [
+	'HH:mm:ss',
+	'H:mm:ss',
+	'mm:ss',
+	'm:ss',
+	'ss',
+	's'
+	]
+
+	for format in formats:
+		try:
+			print(format)
+			time = arrow.get(time, format)
+			break
+		except ParserError:
+			continue
+	return time
+	
+
+def cut_video(startTime, endTime, filename):
+	outfile = 'out_' + filename
+	os.system('ffmpeg -i %s -ss %s -t %s -async 1 %s' % (filename, startTime, endTime, outfile))
+
+
+def download_video(url):
+	cmd = ['youtube-dl', url, '--get-filename', '--restrict-filenames']
+	output = subprocess.Popen(cmd, stdout = subprocess.PIPE).stdout.read()
+	output = output.decode('utf-8')
+	
+
+	output = output.split('\r\n')
+	assert(len(output) == 1)
+	filename = output[0]
+	filename = filename.strip()
+	#output = subprocess.Popen('youtube-dl %s --get-filename' % url, stdout = subprocess.PIPE).stdout.read()
+	
+	print(output)
+
+	if (not os.path.isfile(filename)):
+		cmd = ['youtube-dl', url, '--restrict-filenames']
+		output = subprocess.Popen(cmd, stdout = subprocess.PIPE).stdout.read()
+	
+	return filename
+
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument('url', help='The video url for downloading')
+	parser.add_argument('st', help='The start time')
+	parser.add_argument('et', help='The end time')
+	args = parser.parse_args()
+
+	startTime = args.st
+	startTime = get_time(startTime)
+	endTime = args.et
+	endTime = get_time(endTime)
+
+	print ('Start = %s, end = %s' % (startTime, endTime))
+
+	assert(endTime > startTime)
+
+	#duration = endTime - startTime
+	#print(duration)
+
+	startTime = startTime.datetime.strftime('%H:%M:%S')
+	endTime = endTime.datetime.strftime('%H:%M:%S')
+	print ('Start = %s, end = %s' % (startTime, endTime))
+
+	filename = download_video(args.url)
+	cut_video(startTime, endTime, filename)
+
+
